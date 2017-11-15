d563a34cc4891ec1594a387c9efcad4dc85320f6
Jonas Borg <jonas.borg@netgain.se>
2017-11-15 14:07:17 +0100

    asd

--- jsonextraction.js
+++ jsonextraction.js
@@ -1,40 +1,23 @@
-let cheerio = require('cheerio');
-let jsonframe = require('jsonframe-cheerio');
+let cheerio = require('cheerio')
+let $ = cheerio.load('our html page url here')
 
-let $ = cheerio.load('scrape.html');
-jsonframe($); // initializes the plugin
+var companiesList = [];
 
-var frame = {
-	"companies": {           // setting the parent item as "companies"
-		"selector": ".item",    // defines the elements to search for
-		"data": [{              // "data": [{}] defines a list of items
-			"name": ".header [itemprop=name]",          // inline selector defining "name" so "company"."name"
-			"description": ".header [rel=description]", // inline selector defining "description" as "company"."description"
-			"url": {                                    // defining "url" by an attribute with "attr" and "selector" in an object
-				"selector": ".header [itemprop=name]",      // is actually the same as the inline selector
-				"attr": "href"                              // the attribute name to retrieve
-			},
-			"contact": {                                // set up a parent "contact" element as "company"."contact"
-				"selector": ".contact",                 // defines the element to search for
-				"data": {                               // defines the data which "contact" will contain
-					"telephone": {                          // using "type" to use "telephone" parser to extract only the telephone
-						"selector": "[itemprop=telephone]",     // simple selector for "telephone"                
-						"type": "telephone"                     // using "telephone" plugin parser
-					},
-					"employee": {                           // setting a parent node "employee" as "company"."contact"."employee"
-						"name": "[itemprop=employeeName]",          // inline selector defining "name"
-						"jobTitle": "[itemprop=employeeJobTitle]",  // inline selector defining "jobtitle"
-						"email": {                          // using "type" to use "email" parser to extract only the email
-							"selector": "[itemprop=email]",     // simple selector for "email"
-							"type": "email"                     // using "email" plugin parser
-						}
-					}
-				}
-			}
-		}]
-	}
+// For each .item, we add all the structure of a company to the companiesList array
+// Don't try to understand what follows because we will do it differently.
+$('.list.items .item').each(function(index, element){
+	companiesList[index] = {};
+	var header = $(element).find('.header');
+	companiesList[index]['name'] = $(header).find('[itemprop=name]').text();
+	companiesList[index]['description'] = $(header).find('[rel=description]').text();
+	companiesList[index]['url'] = $(header).find('.header [itemprop=name] a').getAttribute('href');
+	var contact = $(element).find('.contact');
+	companiesList[index]['contact'] = {};
+	companiesList[index]['contact']['telephone'] = $(contact).find('[itemprop=telephone]').text();
+	companiesList[index]['contact']['employee'] = {};
+	companiesList[index]['contact']['employee']['name'] = $(contact).find('[itemprop=employeeName]').text();
+	companiesList[index]['contact']['employee']['jobTitle'] = $(contact).find('[itemprop=employeeJobTitle]').text();
+	companiesList[index]['contact']['employee']['email'] = $(contact).find('[itemprop=email]').text();
+});
 
-};
-
-var companiesList = $('.list.items').scrape(frame);
 console.log(companiesList); // Output the data in the terminal
