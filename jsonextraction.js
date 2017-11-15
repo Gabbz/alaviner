08734c25895251f0fbc925f0c20d3cde3d34101c
Jonas Borg <jonas.borg@netgain.se>
2017-11-15 14:24:25 +0100

    asd

--- jsonextraction.js
+++ jsonextraction.js
@@ -6,8 +6,10 @@ var companiesList = [];
 
 // For each .item, we add all the structure of a company to the companiesList array
 // Don't try to understand what follows because we will do it differently.
-/*$('.list.items .item').each(function(index, element){
-	companiesList[index] = {};
+//$('.list.items .item').each(function(index, element){
+
+
+	/*companiesList[index] = {};
 	var header = $(element).find('.header');
 	companiesList[index]['name'] = $(header).find('[itemprop=name]').text();
 	companiesList[index]['description'] = $(header).find('[rel=description]').text();
@@ -18,8 +20,14 @@ var companiesList = [];
 	companiesList[index]['contact']['employee'] = {};
 	companiesList[index]['contact']['employee']['name'] = $(contact).find('[itemprop=employeeName]').text();
 	companiesList[index]['contact']['employee']['jobTitle'] = $(contact).find('[itemprop=employeeJobTitle]').text();
-	companiesList[index]['contact']['employee']['email'] = $(contact).find('[itemprop=email]').text();
-});*/
+	companiesList[index]['contact']['employee']['email'] = $(contact).find('[itemprop=email]').text();*/
+
+//});
+
+
+$('.list.items .item a').each(function(index, element){
+	companiesList.push( {href: $(element).getAttribute('href') } );
+});
 
 console.log(companiesList); // Output the data in the terminal
 
