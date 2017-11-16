21722d217689fd7be6ae67d5ae1bb8074f7781a0
Filip Ramstedt <magralool@gmail.com>
2017-11-16 15:00:26 +0100

    removed 0 from url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.get('0/api/v1/endpoint/country/:country', function (req, res) {
+app.get('/api/v1/endpoint/country/:country', function (req, res) {
   res.send({
     params: req.params,
     query: req.query
