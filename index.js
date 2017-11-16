3813baae2eb9a8352199d9096c22f797cd5e65b8
Filip Ramstedt <magralool@gmail.com>
2017-11-16 14:58:22 +0100

    asdf

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
+
+app.get('0/api/v1/endpoint/country/:country', function (req, res) {
+  res.send({
+    params: req.params,
+    query: req.query
+  });
+});
  
-app.listen(3000)
+app.listen(3000);
